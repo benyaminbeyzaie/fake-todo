653c4fc0953744cce6430eca6ea5aec07f450269
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2022-06-09 15:09:46 +0430

    Initial Commit

---
+++ Shared/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  Shared
+//
+//  Created by Benyamin on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
